fb5b8ac7f25fa44845d8d82a872ee20f16c29890
Matthew Unrath <matthew.unrath@gmail.com>
2024-12-17 21:40:13 -0800

    Day 11 part one

--- 2024/src/Day_11.cs
+++ 2024/src/Day_11.cs
@@ -8,11 +8,47 @@ public class Day_11 : IDay
         _logger = factory.CreateLogger<Day_11>();
     }
 
+    // Intuition: since the stones are splitting in-place, we want a linked list.
+    // For every blink, this is O(N), but N grows each time. So pessimistically... ≈O(N*2^B)?
     public long Solve(string inputPath)
     {
-        string[] lines = File.ReadAllLines(inputPath);
+        LinkedList<long> stones = new(File.ReadAllLines(inputPath).Single().Split(" ").Select(long.Parse));
 
-        return 0;
+        for (int i = 0; i < 25; i++)
+        {
+            BlinkStones(stones);
+        }
+
+        return stones.Count;
+    }
+
+    private static void BlinkStones(LinkedList<long> stones)
+    {
+        LinkedListNode<long>? curr = stones.First;
+
+        while (curr != null)
+        {
+            if (curr.Value == 0)
+            {
+                curr.Value = 1;
+            }
+            else if (curr.Value.ToString().Length % 2 == 0)
+            {
+                string parent = curr.Value.ToString();
+                long firstHalf = long.Parse(parent[..(parent.Length / 2)]);
+                long secondHalf = long.Parse(parent[(parent.Length / 2)..]);
+
+                curr.Value = firstHalf;
+                stones.AddAfter(curr, secondHalf);
+                // Skip the second-half stone since it was just created
+                curr = curr.Next;
+            }
+            else
+            {
+                curr.Value *= 2024;
+            }
+            curr = curr.Next;
+        }
     }
 
     public long SolvePartTwo(string inputPath)
--- 2024/test/unit/Day_11Test.cs
+++ 2024/test/unit/Day_11Test.cs
@@ -11,7 +11,7 @@ public class Day_11Test
 
         long result = solver.Solve("day_11_sample.txt");
 
-        Assert.Equal(11, result);
+        Assert.Equal(7, result);
     }
 
     [Fact]
