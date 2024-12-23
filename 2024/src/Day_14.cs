c2df634ac2f6ea63f43caf4ecb8a05ef5b272b46
Matthew Unrath <matthew.unrath@gmail.com>
2024-12-22 23:27:38 -0800

    Day 14 part one

--- 2024/src/Day_14.cs
+++ 2024/src/Day_14.cs
@@ -8,11 +8,113 @@ public class Day_14 : IDay
         _logger = factory.CreateLogger<Day_14>();
     }
 
+    // Intuition: Since the robot wraps around, it's the same as mod.
+    // This can be calculated in O(N) time for N robots.
     public long Solve(string inputPath)
     {
         string[] lines = File.ReadAllLines(inputPath);
+        var robots = ReadRobots(lines);
 
-        return 0;
+        int[] quadrantTotals = new int[4];
+
+        foreach (Robot r in robots)
+        {
+            Quadrant q = r.GetQuadrant(seconds: 100);
+            _logger.LogInformation("Placed robot in quadrant {q}", q.ToString());
+            if (q == Quadrant.None)
+            {
+                continue;
+            }
+
+            quadrantTotals[(int)q] += 1;
+        }
+
+        return quadrantTotals.Aggregate(1, (acc, x) => acc * x);
+    }
+
+    private List<Robot> ReadRobots(string[] lines)
+    {
+        List<Robot> robots = [];
+        //p=0,4 v=3,-3
+        foreach (string line in lines)
+        {
+            var split = line.Split(" ");
+            string[] position = split[0].Split("=")[1].Split(",");
+            string[] velocity = split[1].Split("=")[1].Split(",");
+            robots.Add(new()
+            {
+                X = int.Parse(position[0]),
+                Y = int.Parse(position[1]),
+                Dx = int.Parse(velocity[0]),
+                Dy = int.Parse(velocity[1])
+            });
+        }
+
+        return robots;
+    }
+
+    public enum Quadrant
+    {
+        None = -1,
+        UpperLeft = 0,
+        UpperRight = 1,
+        LowerLeft = 2,
+        LowerRight = 3
+    };
+
+    private class Robot
+    {
+        public int X;
+        public int Y;
+        public int Dx;
+        public int Dy;
+
+        private const int _width = 101;
+        private const int _height = 103;
+
+        public Quadrant GetQuadrant(int seconds)
+        {
+            (int fx, int fy) = FinalPosition(seconds);
+            if (fx == _width / 2 || fy == _height / 2)
+            {
+                return Quadrant.None;
+            }
+            else if (fx <= _width / 2 && fy <= _height / 2)
+            {
+                return Quadrant.UpperLeft;
+            }
+            else if (fx > _width / 2 && fy <= _height / 2)
+            {
+                return Quadrant.UpperRight;
+            }
+            else if (fx <= _width / 2 && fy > _height / 2)
+            {
+                return Quadrant.LowerLeft;
+            }
+            else
+            {
+                return Quadrant.LowerRight;
+            }
+        }
+
+        private (int x, int y) FinalPosition(int seconds)
+        {
+            (int fx, int fy) = (
+                (X + Dx * seconds) % _width,
+                (Y + Dy * seconds) % _height
+            );
+
+            if (fx < 0)
+            {
+                fx += _width;
+            }
+            if (fy < 0)
+            {
+                fy += _height;
+            }
+
+            return (fx, fy);
+        }
     }
 
     public long SolvePartTwo(string inputPath)
--- 2024/test/unit/Day_14Test.cs
+++ 2024/test/unit/Day_14Test.cs
@@ -4,14 +4,14 @@ using Microsoft.Extensions.Logging;
 public class Day_14Test
 {
     [Fact]
-    public void Solve_()
+    public void Solve_BucketsRobotsIntoCorrectQuadrants()
     {
         using ILoggerFactory factory = LoggerFactory.Create(builder => { });
         Day_14 solver = new(factory);
 
         long result = solver.Solve("day_14_sample.txt");
 
-        Assert.Equal(11, result);
+        Assert.Equal(12, result);
     }
 
     [Fact]
