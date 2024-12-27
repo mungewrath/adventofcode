325cd7f10924a4f4799a697f258ebdb5c86de198
Matthew Unrath <matthew.unrath@gmail.com>
2024-12-27 15:01:17 -0800

    Day 19 part one

--- 2024/src/Day_19.cs
+++ 2024/src/Day_19.cs
@@ -1,18 +1,83 @@
+using System.Data.SqlTypes;
+using System.Text;
 using Microsoft.Extensions.Logging;
 
 public class Day_19 : IDay
 {
     private ILogger<Day_19> _logger;
+    private Dictionary<string, bool> _memoized;
+
     public Day_19(ILoggerFactory factory)
     {
         _logger = factory.CreateLogger<Day_19>();
+        _memoized = [];
     }
 
+
+    // Intuition: Feels like a dynamic programming case, so solve using recursion (DFS?).
     public long Solve(string inputPath)
     {
         string[] lines = File.ReadAllLines(inputPath);
+        (List<string> towels, List<string> designsNeeded) = ReadRequest(lines);
 
-        return 0;
+        return designsNeeded.Select(d => IsPossible(d, towels, new StringBuilder())).Count(p => p == true);
+    }
+
+    private (List<string> towels, List<string> designsNeeded) ReadRequest(string[] lines)
+    {
+        var towels = lines[0].Split(", ").OrderByDescending(s => s.Length).ToList();
+
+        var designsNeeded = lines[2..].ToList();
+
+        return (towels, designsNeeded);
+    }
+
+    private bool IsPossible(string design, List<string> towels, StringBuilder sb)
+    {
+        if (_memoized.ContainsKey(design[sb.Length..]))
+        {
+            return _memoized[design[sb.Length..]];
+        }
+
+        if (sb.Length == design.Length)
+        {
+            _memoized.Add(design, true);
+            return true;
+        }
+
+        foreach (string t in towels)
+        {
+            if (sb.Length + t.Length > design.Length)
+            {
+                continue;
+            }
+
+            bool matches = true;
+            for (int i = 0; i < t.Length; i++)
+            {
+                if (design[sb.Length + i] != t[i])
+                {
+                    matches = false;
+                    break;
+                }
+            }
+
+            if (matches)
+            {
+                sb.Append(t);
+                if (IsPossible(design, towels, sb))
+                {
+                    return true;
+                }
+                else
+                {
+                    sb.Remove(sb.Length - t.Length, t.Length);
+                }
+            }
+        }
+
+        _memoized[design[sb.Length..]] = false;
+        return false;
     }
 
     public long SolvePartTwo(string inputPath)
--- 2024/test/unit/Day_19Test.cs
+++ 2024/test/unit/Day_19Test.cs
@@ -4,14 +4,14 @@ using Microsoft.Extensions.Logging;
 public class Day_19Test
 {
     [Fact]
-    public void Solve_()
+    public void Solve_CorrectlyIdentifiesValidDesigns()
     {
         using ILoggerFactory factory = LoggerFactory.Create(builder => { });
         Day_19 solver = new(factory);
 
         long result = solver.Solve("day_19_sample.txt");
 
-        Assert.Equal(11, result);
+        Assert.Equal(6, result);
     }
 
     [Fact]
