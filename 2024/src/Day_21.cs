84ac3a5dfe6bfc3dafe80a47a7b595ae5c36ee42
Matthew Unrath <matthew.unrath@gmail.com>
2024-12-31 14:11:42 -0800

    Day 21 part one

--- 2024/src/Day_21.cs
+++ 2024/src/Day_21.cs
@@ -8,11 +8,180 @@ public class Day_21 : IDay
         _logger = factory.CreateLogger<Day_21>();
     }
 
+    // Intuition: This has a lot of layers, but at its core it's translating button pushes in O(N) time.
+    // Work backwards, starting from the robot pressing the numpad, to the robot controlling it, until we get to us at the top level.
     public long Solve(string inputPath)
     {
-        string[] lines = File.ReadAllLines(inputPath);
+        string[] codes = File.ReadAllLines(inputPath);
 
-        return 0;
+        List<int> complexities = [];
+
+        foreach (string code in codes)
+        {
+            var dpad1 = TranslateCodeToLevel1Robot(code);
+            var dpad2 = TranslateDpadToDpad(dpad1);
+            var human = TranslateDpadToDpad(dpad2);
+
+            _logger.LogInformation("{dpad1}", dpad1);
+            _logger.LogInformation("{dpad2}", dpad2);
+            _logger.LogInformation("{human}", human);
+
+            complexities.Add(human.Count * int.Parse(code[..^1]));
+        }
+
+
+        return complexities.Sum();
+    }
+
+    private List<char> TranslateCodeToLevel1Robot(string code)
+    {
+        List<char> dpad = [];
+        Dictionary<char, (int x, int y)> coordsForButtons = new(){
+            {'7', (0, 0) },
+            {'8', (1, 0) },
+            {'9', (2, 0) },
+            {'4', (0, 1) },
+            {'5', (1, 1) },
+            {'6', (2, 1) },
+            {'1', (0, 2) },
+            {'2', (1, 2) },
+            {'3', (2, 2) },
+            {'0', (1, 3) },
+            {'A', (2, 3) },
+            {'X', (0,3)} // Illegal spot
+        };
+
+        (int x, int y) = coordsForButtons['A'];
+
+        foreach (char c in code)
+        {
+            (int nx, int ny) = coordsForButtons[c];
+            // Order is important so robot doesn't go over an empty space and "panic"
+
+            // TODO: somehow this order influences the dpads later.
+            // Prefer this order and flip if it's not legal.
+
+            List<char> stepsForNextButton = [];
+            bool enteredIllegalSpot = false;
+
+            while (nx < x)
+            {
+                stepsForNextButton.Add('<');
+                x--;
+                if ((x, y) == coordsForButtons['X'])
+                {
+                    enteredIllegalSpot = true;
+                }
+            }
+            while (ny > y)
+            {
+                stepsForNextButton.Add('v');
+                y++;
+                if ((x, y) == coordsForButtons['X'])
+                {
+                    enteredIllegalSpot = true;
+                }
+            }
+            while (ny < y)
+            {
+                stepsForNextButton.Add('^');
+                y--;
+                if ((x, y) == coordsForButtons['X'])
+                {
+                    enteredIllegalSpot = true;
+                }
+            }
+            while (nx > x)
+            {
+                stepsForNextButton.Add('>');
+                x++;
+                if ((x, y) == coordsForButtons['X'])
+                {
+                    enteredIllegalSpot = true;
+                }
+            }
+
+            if (enteredIllegalSpot)
+            {
+                stepsForNextButton.Reverse();
+            }
+
+            stepsForNextButton.Add('A');
+            dpad.AddRange(stepsForNextButton);
+        }
+
+        return dpad;
+    }
+
+    private List<char> TranslateDpadToDpad(List<char> dpad1)
+    {
+        List<char> dpad2 = [];
+        Dictionary<char, (int x, int y)> coordsForButtons = new(){
+            {'^', (1, 0) },
+            {'A', (2, 0) },
+            {'<', (0, 1) },
+            {'v', (1, 1) },
+            {'>', (2, 1) },
+            {'X', (0, 0) }
+        };
+
+        (int x, int y) = coordsForButtons['A'];
+
+        foreach (char c in dpad1)
+        {
+            (int nx, int ny) = coordsForButtons[c];
+
+            // Order is important so robot doesn't go over an empty space and "panic"
+
+            List<char> stepsForNextButton = [];
+            bool enteredIllegalSpot = false;
+
+            while (nx > x)
+            {
+                stepsForNextButton.Add('>');
+                x++;
+                if ((x, y) == coordsForButtons['X'])
+                {
+                    enteredIllegalSpot = true;
+                }
+            }
+            while (ny > y)
+            {
+                stepsForNextButton.Add('v');
+                y++;
+                if ((x, y) == coordsForButtons['X'])
+                {
+                    enteredIllegalSpot = true;
+                }
+            }
+            while (ny < y)
+            {
+                stepsForNextButton.Add('^');
+                y--;
+                if ((x, y) == coordsForButtons['X'])
+                {
+                    enteredIllegalSpot = true;
+                }
+            }
+            while (nx < x)
+            {
+                stepsForNextButton.Add('<');
+                x--;
+                if ((x, y) == coordsForButtons['X'])
+                {
+                    enteredIllegalSpot = true;
+                }
+            }
+
+            if (enteredIllegalSpot)
+            {
+                stepsForNextButton.Reverse();
+            }
+            stepsForNextButton.Add('A');
+            dpad2.AddRange(stepsForNextButton);
+        }
+
+        return dpad2;
     }
 
     public long SolvePartTwo(string inputPath)
--- 2024/test/unit/Day_21Test.cs
+++ 2024/test/unit/Day_21Test.cs
@@ -4,14 +4,14 @@ using Microsoft.Extensions.Logging;
 public class Day_21Test
 {
     [Fact]
-    public void Solve_()
+    public void Solve_TranslatesToHumanPressesOptimally()
     {
         using ILoggerFactory factory = LoggerFactory.Create(builder => { });
         Day_21 solver = new(factory);
 
         long result = solver.Solve("day_21_sample.txt");
 
-        Assert.Equal(11, result);
+        Assert.Equal(126384, result);
     }
 
     [Fact]
